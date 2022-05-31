31ed2f0f0b79d3de8d8d6a7dfdda8b69f96a9e7d
ASureeva <annasureeva3955@gmail.com>
2022-06-01 01:01:14 +0300

    Edited readme

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -34,6 +34,7 @@ import OpenChat from "./pages/OpenChat";
 import Comment from "./pages/Comment";
 import EditImageProfile from "./pages/EditImageProfile";
 import EditThreadImage from "./components/EditThreadImage";
+import {fetchToken} from "./components/Auth";
 
 
 function App() {
@@ -44,54 +45,59 @@ function App() {
           <Category />
           <Container>
             <Routes>
-              <Route path="/" element = {<Main />}/>
-              <Route path="/auth/login" element = {<Login />}/>
-              <Route path="/profile/get_profile_info" element = {<Profile />}/>
-              <Route path="/update_password_info" element = {<EditPassword />}/>
-              <Route path="/update_profile_info" element = {<ProfileEdit />}>
-                  <Route path=":nickname" element={<ProfileEdit />}/>
-              </Route>
-              <Route path="/update_profile_image" element = {<EditImageProfile />}>
-                  <Route path=":nickname" element={<EditImageProfile />}/>
-              </Route>
-              <Route path="/auth/register" element = {<Registration />}/>
-              <Route path="/auth/oauth" element={<GetAuth/>}/>
-              <Route path="/chat/get_chats" element = {<Chats />}/>
-              <Route path="/friendlist/get_friends" element = {<Friendlist/>}/>
-              <Route path="/recommended_friends" element={<RecommFriends/>}/>
-              <Route path="/search_friend" element = {<SearchFriend/>}/>
-              <Route path="*" element={<NoPage />} />
-              <Route path="default_profile/get_profile_info/" element={<DefaultProfile/>}>
-                <Route path=":nickname" element={<DefaultProfile />}/>
-              </Route>
-              <Route path="/chat/open_chat/" element = {<OpenChat />}>
-                  <Route path=":nickname" element = {<OpenChat />}/>
-              </Route>
-              <Route path="/category/subscriptions" element={<AllCategory/>}/>
-              <Route path="/category/my_category" element={<CreatedCategories/>}/>
-              <Route path="/category/update_category" element={<UpdateCategory/>}>
-                     <Route path=":category" element={<UpdateCategory/>}/>
-              </Route>
-              <Route path="/category/create_category" element={<AddCategory/>}/>
-              <Route path="/category/search" element={<SearchCategory/>}/>
-              <Route path="/category/recommended_categories" element={<RecommCategory/>}/>
-              <Route path="/category/" element = {<DefaultCategory />}>
-                  <Route path=":category" element = {<DefaultCategory />}/>
-                  <Route path=":category" element = {<DefaultCategory />}/>
-              </Route>
-              <Route path="/thread/create_thread" element={<AddThread/>}>
-                   <Route path=":category" element={<AddThread/>}/>
-              </Route>
-              <Route path="/thread/" element={<DefaultThread/>}>
-                   <Route path=":thread" element={<DefaultThread/>}/>
-              </Route>
-              <Route path="/thread/update" element={<UpdateThread/>}>
-                   <Route path=":thread" element={<UpdateThread/>}/>
-              </Route>
-              <Route path="/comment/update/" element={<UpdateComment/>}>
-                  <Route path=":comment" element={<UpdateComment/>}/>
-              </Route>
-              <Route path="/lol/" element={<Comment/>}/>
+                <Route path="/" element = {<Main />}/>
+                <Route path="/auth/login" element = {<Login />}/>
+                <Route path="/auth/register" element = {<Registration />}/>
+                <Route path="/auth/oauth" element={<GetAuth/>}/>
+                <Route path="*" element={<NoPage />} />
+                {fetchToken() ? (
+                    <>
+                        <Route path="default_profile/get_profile_info/" element={<DefaultProfile/>}>
+                            <Route path=":nickname" element={<DefaultProfile />}/>
+                        </Route>
+                        <Route path="/search_friend" element = {<SearchFriend/>}/>
+                        <Route path="/category/search" element={<SearchCategory/>}/>
+                        <Route path="/category/" element = {<DefaultCategory />}>
+                            <Route path=":category" element = {<DefaultCategory />}/>
+                        </Route>
+                        <Route path="/thread/" element={<DefaultThread/>}>
+                            <Route path=":thread" element={<DefaultThread/>}/>
+                        </Route>
+                        <Route path="/profile/get_profile_info" element = {<Profile />}/>
+                        <Route path="/update_password_info" element = {<EditPassword />}/>
+                        <Route path="/update_profile_info" element = {<ProfileEdit />}>
+                            <Route path=":nickname" element={<ProfileEdit />}/>
+                        </Route>
+                        <Route path="/update_profile_image" element = {<EditImageProfile />}>
+                            <Route path=":nickname" element={<EditImageProfile />}/>
+                        </Route>
+                        <Route path="/chat/get_chats" element = {<Chats />}/>
+                        <Route path="/friendlist/get_friends" element = {<Friendlist/>}/>
+                        <Route path="/recommended_friends" element={<RecommFriends/>}/>
+                        <Route path="/chat/open_chat/" element = {<OpenChat />}>
+                            <Route path=":nickname" element = {<OpenChat />}/>
+                        </Route>
+                        <Route path="/category/subscriptions" element={<AllCategory/>}/>
+                        <Route path="/category/my_category" element={<CreatedCategories/>}/>
+                        <Route path="/category/update_category" element={<UpdateCategory/>}>
+                            <Route path=":category" element={<UpdateCategory/>}/>
+                        </Route>
+                        <Route path="/category/create_category" element={<AddCategory/>}/>
+                        <Route path="/category/recommended_categories" element={<RecommCategory/>}/>
+                        <Route path="/thread/create_thread" element={<AddThread/>}>
+                            <Route path=":category" element={<AddThread/>}/>
+                        </Route>
+                        <Route path="/thread/update" element={<UpdateThread/>}>
+                            <Route path=":thread" element={<UpdateThread/>}/>
+                        </Route>
+                        <Route path="/comment/update/" element={<UpdateComment/>}>
+                            <Route path=":comment" element={<UpdateComment/>}/>
+                        </Route>
+                    </>
+                ):(
+                    <>
+                    </>
+                )}
             </Routes>
           </Container>
         </Router>
