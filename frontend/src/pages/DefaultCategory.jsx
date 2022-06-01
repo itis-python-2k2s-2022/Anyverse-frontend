23e0c1138c1a73929f127e9db49d0d6d0cf7b1a8
EricaZaripova <ericaz@mail.ru>
2022-06-01 07:45:34 +0300

    fixed photo in categories part two

--- frontend/src/pages/DefaultCategory.jsx
+++ frontend/src/pages/DefaultCategory.jsx
@@ -93,7 +93,8 @@ const DefaultCategory = () => {
             block1.innerText = category_info.description
             const block2 = document.getElementById("category_subscriptions")
             block2.innerText = category_info.subscriptions
-            setSrcImage(`${process.env.REACT_APP_API_URL}` + category_info.image);
+            setSrcImage(`${process.env.REACT_APP_API_URL}/` + category_info.image);
+            console.log(srcImage)
 
         })
           .catch(function (error) {
